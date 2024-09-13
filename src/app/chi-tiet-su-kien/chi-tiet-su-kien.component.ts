307a690c17d4e5ae823b932e40254cd9f7591577
dangkhoab1910240 <khoab1910240@gmail.com>
2024-09-13 21:50:58 +0700

    Sửa báo lỗi

--- src/app/chi-tiet-su-kien/chi-tiet-su-kien.component.ts
+++ src/app/chi-tiet-su-kien/chi-tiet-su-kien.component.ts
@@ -18,14 +18,24 @@ import { TinhThanhChiTiet } from '../Models/tinhThanh/tinh-thanh-chi-tiet';
   selector: 'app-chi-tiet-su-kien',
   templateUrl: './chi-tiet-su-kien.component.html',
   styleUrls: ['./chi-tiet-su-kien.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ChiTietSuKienComponent implements OnInit {
   eventId: number = -1;
   userId!: number;
   event!: SuKien;
   hoatDong: HoatDong[] = [];
-  constructor(private tinhThanhService: TinhThanhService,private spinner: NgxSpinnerService,private registrationService: RegistrationService,private userService: UserService,private router: Router,private datePipe: DatePipe ,private route: ActivatedRoute,private suKienService: SuKienService,private hoatDongService: HoatDongService) { }
+  constructor(
+    private tinhThanhService: TinhThanhService,
+    private spinner: NgxSpinnerService,
+    private registrationService: RegistrationService,
+    private userService: UserService,
+    private router: Router,
+    private datePipe: DatePipe,
+    private route: ActivatedRoute,
+    private suKienService: SuKienService,
+    private hoatDongService: HoatDongService
+  ) {}
   eventDays: string[] = [];
   relatedEvents: SuKien[] = [];
   thamGiaChua: Boolean = false;
@@ -33,7 +43,7 @@ export class ChiTietSuKienComponent implements OnInit {
   registration: Registration | null = null;
   paymentMethod: number = 1;
   refuseMessage: string = '';
-  tinhThanhChiTiet !: TinhThanhChiTiet;
+  tinhThanhChiTiet!: TinhThanhChiTiet;
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -46,73 +56,98 @@ export class ChiTietSuKienComponent implements OnInit {
     this.checkExistByUserIdAndEventId();
   }
   checkExistByUserIdAndEventId() {
-    this.userService.getInfoByUsername(JSON.parse(localStorage.getItem('username')!)).subscribe({
-      next:(response: User) => {
-        this.userId = response.id;
+    this.userService
+      .getInfoByUsername(JSON.parse(localStorage.getItem('username')!))
+      .subscribe({
+        next: (response: User) => {
+          this.userId = response.id;
 
-        this.registrationService.checkExist(response.id,this.eventId).subscribe({
-          next:(response: Boolean) => {
-            this.thamGiaChua = response;
-          },
-          error: (error) => {
-            console.log(error);
-          }
-        })
-        this.getRegistrationByUserIdAndEventId();
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    })
-  }
-  getRegistrationByUserIdAndEventId()  {
-    this.registrationService.getRegistrationByUserIdAndEventId(this.userId,this.eventId).subscribe({
-      next: (response: Registration) => {
-        this.registration = response;
-      },
-      error: (error) => {
-
-      }
-    })
-  }
-  dangKySuKien() {
-    this.spinner.show();
-    let payString = ''
-    if(confirm("Bạn đăng ký sự kiện với phương thức thanh toán là "+(this.paymentMethod == 1 ? "VNPay" : "Thanh toán trực tiếp")+" ! Bạn có muốn tiếp tục không.")) {
-        this.registrationService.registerEvent({
-          "users" : {
-              "id" :this.userId
-          },
-          "event" : {
-              "id":this.eventId
-          },
-          "paymentMethod" : this.paymentMethod
-      }).subscribe({
-        next: (response: string) => {
-          
-          this.spinner.hide();
-          if(this.paymentMethod == 1) {
-            window.location.href = response;
-          } else {
-            alert("Đăng ký sự kiện thành công!");
-            this.thamGiaChua = true;
-            this.getRegistrationByUserIdAndEventId();
-            this.fetchEventDetails();
-          }
-          
+          this.registrationService
+            .checkExist(response.id, this.eventId)
+            .subscribe({
+              next: (response: Boolean) => {
+                this.thamGiaChua = response;
+              },
+              error: (error) => {
+                console.log(error);
+              },
+            });
+          this.getRegistrationByUserIdAndEventId();
         },
         error: (error) => {
           console.log(error);
-          this.spinner.hide();
-        }
-      })
+        },
+      });
+  }
+  getRegistrationByUserIdAndEventId() {
+    this.registrationService
+      .getRegistrationByUserIdAndEventId(this.userId, this.eventId)
+      .subscribe({
+        next: (response: Registration) => {
+          this.registration = response;
+        },
+        error: (error) => {},
+      });
+  }
+  dangKySuKien() {
+    this.spinner.show();
+    let payString = '';
+    if (
+      confirm(
+        'Bạn đăng ký sự kiện với phương thức thanh toán là ' +
+          (this.paymentMethod == 1 ? 'VNPay' : 'Thanh toán trực tiếp') +
+          ' ! Bạn có muốn tiếp tục không.'
+      )
+    ) {
+      this.registrationService
+        .registerEvent({
+          users: {
+            id: this.userId,
+          },
+          event: {
+            id: this.eventId,
+          },
+          paymentMethod: this.paymentMethod,
+        })
+        .subscribe({
+          next: (response: string) => {
+            this.spinner.hide();
+            if (this.paymentMethod == 1) {
+              window.location.href = response;
+            } else {
+              alert('Đăng ký sự kiện thành công!');
+              this.thamGiaChua = true;
+              this.getRegistrationByUserIdAndEventId();
+              this.fetchEventDetails();
+            }
+          },
+          error: (error) => {
+            this.spinner.hide();
+
+            let errorMessage = '';
+
+            // Kiểm tra nếu error.error là chuỗi, chuyển đổi nó thành đối tượng JSON
+            if (typeof error.error === 'string') {
+              try {
+                const parsedError = JSON.parse(error.error);
+                errorMessage = parsedError.message || 'Có lỗi xảy ra.';
+              } catch (e) {
+                errorMessage = 'Không thể phân tích lỗi từ server.';
+              }
+            } else {
+              // Nếu error.error là đối tượng JSON, lấy thuộc tính message
+              errorMessage = error.error.message || 'Có lỗi xảy ra.';
+            }
+
+           alert(errorMessage);
+          },
+        });
     } else {
       this.spinner.hide();
     }
-    
   }
   getEventId() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.eventId = params['id'];
       // Now you can use this.eventId in your component logic
       console.log('Event ID:', this.eventId);
@@ -126,81 +161,98 @@ export class ChiTietSuKienComponent implements OnInit {
         const startDate = new Date(this.event.startDateTime);
         const endDate = new Date(this.event.endDateTime);
         this.hanDangKy = subDays(startDate, 1);
-        this.getTop5SuKienByOrganizerIdExcludingEventId(response.organizer.id,this.eventId);
-      // Lặp qua từng ngày và thêm vào mảng nếu chưa tồn tại
-      if(startDate && endDate) {
-        while (startDate <= endDate) {
-          const formattedDate = this.datePipe.transform(startDate, 'yyyy-MM-dd');
-          if (!this.eventDays.includes(formattedDate!)) {
-            this.eventDays.push(formattedDate!);
+        this.getTop5SuKienByOrganizerIdExcludingEventId(
+          response.organizer.id,
+          this.eventId
+        );
+        // Lặp qua từng ngày và thêm vào mảng nếu chưa tồn tại
+        if (startDate && endDate) {
+          while (startDate <= endDate) {
+            const formattedDate = this.datePipe.transform(
+              startDate,
+              'yyyy-MM-dd'
+            );
+            if (!this.eventDays.includes(formattedDate!)) {
+              this.eventDays.push(formattedDate!);
+            }
+            startDate.setDate(startDate.getDate() + 1);
           }
-          startDate.setDate(startDate.getDate() + 1);
         }
-      }
-      this.getHoatDongByEventIdAndDate(this.eventDays[0]);
+        this.getHoatDongByEventIdAndDate(this.eventDays[0]);
       },
-      error: (error) => {
-        
-      }
-    })
-  }
-  goToChiTietSuKien(id: number) {
-    this.router.navigateByUrl('/chitietsukien', { skipLocationChange: false }).then(() => {
-      this.router.navigate(['/chitietsukien', id]);
+      error: (error) => {},
     });
   }
-  getTop5SuKienByOrganizerIdExcludingEventId(organizerId: number,eventId: number) {
-    this.suKienService.getTop5SuKienByOrganizerIdExcludingEventId(organizerId,eventId).subscribe({
-      next: (response: SuKien[]) => {
-        this.relatedEvents = response;
-      }
-    })
+  goToChiTietSuKien(id: number) {
+    this.router
+      .navigateByUrl('/chitietsukien', { skipLocationChange: false })
+      .then(() => {
+        this.router.navigate(['/chitietsukien', id]);
+      });
+  }
+  getTop5SuKienByOrganizerIdExcludingEventId(
+    organizerId: number,
+    eventId: number
+  ) {
+    this.suKienService
+      .getTop5SuKienByOrganizerIdExcludingEventId(organizerId, eventId)
+      .subscribe({
+        next: (response: SuKien[]) => {
+          this.relatedEvents = response;
+        },
+      });
   }
   getHoatDongByEventIdAndDate(date: string) {
-    if(date == "") {
+    if (date == '') {
       date = this.eventDays[0];
     }
     console.log(date);
-    this.hoatDongService.getHoatDongByEventIdAndDate(this.eventId,date).subscribe({
-      next: (response: HoatDong[]) => {
-        console.log(response);
-        this.hoatDong =response;
-      },
-      error: (error) => {
-
-      }
-    })
+    this.hoatDongService
+      .getHoatDongByEventIdAndDate(this.eventId, date)
+      .subscribe({
+        next: (response: HoatDong[]) => {
+          console.log(response);
+          this.hoatDong = response;
+        },
+        error: (error) => {},
+      });
   }
   onSubmit() {
     if (this.refuseMessage.trim() === '') {
       alert('Vui lòng nhập lý do không tham dự.');
     } else {
-      this.registrationService.getRegistrationByUserIdAndEventId(this.userId,this.eventId).subscribe({
-        next: (response: Registration) => {
-          this.registrationService.updateById2(response.id, this.userId, {"refuseMessage": this.refuseMessage}).subscribe({
-            next: (response) => {
-              alert("Gửi lý do không tham dự sự kiện thành công.");
-              this.refuseMessage = '';
-              this.getRegistrationByUserIdAndEventId();
-              this.fetchEventDetails();
-            },
-            error: (error) => {
-              alert(error.error.message);
-            }
-          })
-        }
-      })
-      
+      this.registrationService
+        .getRegistrationByUserIdAndEventId(this.userId, this.eventId)
+        .subscribe({
+          next: (response: Registration) => {
+            this.registrationService
+              .updateById2(response.id, this.userId, {
+                refuseMessage: this.refuseMessage,
+              })
+              .subscribe({
+                next: (response) => {
+                  alert('Gửi lý do không tham dự sự kiện thành công.');
+                  this.refuseMessage = '';
+                  this.getRegistrationByUserIdAndEventId();
+                  this.fetchEventDetails();
+                },
+                error: (error) => {
+                  alert(error.error.message);
+                },
+              });
+          },
+        });
     }
   }
 
   getEventAddress(event: SuKien) {
     console.log(event);
-    this.tinhThanhService.getDetailsAddressById(this.event.phuongXaId).subscribe({
-      next: (response: any) => {
-
+    this.tinhThanhService
+      .getDetailsAddressById(this.event.phuongXaId)
+      .subscribe({
+        next: (response: any) => {
           this.tinhThanhChiTiet = response.data;
-      }
-    })
+        },
+      });
   }
 }
